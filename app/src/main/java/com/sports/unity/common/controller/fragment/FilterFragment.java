0b1f5e8439a85421ea80badd30e38f393f3cb055
priyadarshi1 <priyadarshi@madmachines.io>
2016-01-27 18:54:33 +0530

    change for filterActivity

---
+++ app/src/main/java/com/sports/unity/common/controller/fragment/FilterFragment.java
@@ -0,0 +1,22 @@
+package com.sports.unity.common.controller.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sports.unity.R;
+
+
+public class FilterFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // initItemList();
+    }
+}
