0aa06e5167139a19701696d165bb637d4e8987c2
MadMachines <arjun@cfeindia.com>
2016-02-12 19:08:20 +0530

    Create cricket player profile info

---
+++ app/src/main/java/com/sports/unity/scoredetails/cricketdetail/PlayerCricketBioDataActivity.java
@@ -0,0 +1,23 @@
+package com.sports.unity.scoredetails.cricketdetail;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.sports.unity.R;
+
+public class PlayerCricketBioDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_cricket_bio_data);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
