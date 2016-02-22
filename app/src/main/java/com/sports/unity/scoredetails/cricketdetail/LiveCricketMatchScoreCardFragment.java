3d0861b18bec8d54e7640ee5626620a5b6cd985b
Arjun <arjun@cfeindia.com>
2016-02-22 14:48:13 +0530

    live and completed match summery activity initialization.

--- app/src/main/java/com/sports/unity/scoredetails/cricketdetail/LiveCricketMatchScoreCardFragment.java
+++ app/src/main/java/com/sports/unity/scoredetails/cricketdetail/LiveCricketMatchScoreCardFragment.java
@@ -1,29 +1,42 @@
 package com.sports.unity.scoredetails.cricketdetail;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sports.unity.R;
 import com.sports.unity.scoredetails.model.CricketScoreCard;
-import com.sports.unity.scoredetails.model.Scorecard;
 import com.sports.unity.scores.ScoreDetailActivity;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class LiveCricketMatchScoreCardFragment extends Fragment implements LivedMatchScoreCardHandler.LiveMatchContentListener{
 
 
+    private ImageView ivDwn;
+    private TextView tvTeamFirstNameAndScore;
+    private TextView tvFirstTeamOver;
+    private TextView tvExtraRunTeamFirst;
+    private TextView tvTotalRunFirstTeam;
+    private TextView tvRunRateFirstTeam;
+    private TextView tvFirstTeamScore;
+    private TextView tvFirstTeamOvers;
+    private ImageView ivDwnSecond;
+    private TextView tvTeamSecondNameAndScore;
+    private TextView tvSecondTeamOver;
+    private TextView tvExtraRunTeamSecond;
+    private TextView tvTotalRunSecondTeam;
+    private TextView tvRunRateSecondTeam;
+    private TextView tvSecondTeamScore;
+    private TextView tvSecondTeamOvers;
     public LiveCricketMatchScoreCardFragment() {
         super();
     }
@@ -41,12 +54,30 @@ public class LiveCricketMatchScoreCardFragment extends Fragment implements Lived
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_cricket_live_match_summery, container, false);
+        View view = inflater.inflate(R.layout.fragment_completed_match_score_card, container, false);
         initView(view);
         return view;
     }
+
+    /* Tis method use to initialization view element of  fragment_completed_match_score_card*/
     private void initView(View view) {
 
+        ivDwn = (ImageView) view.findViewById(R.id.iv_down);
+        tvTeamFirstNameAndScore = (TextView) view.findViewById(R.id.tv_team_first_name);
+        tvFirstTeamOver = (TextView) view.findViewById(R.id.tv_match_over);
+        tvExtraRunTeamFirst = (TextView) view.findViewById(R.id.tv_extra_run_team_first);
+        tvTotalRunFirstTeam = (TextView) view.findViewById(R.id.tv_total_run_first_team);
+        tvRunRateFirstTeam = (TextView) view.findViewById(R.id.tv_run_rate_first_team);
+        tvFirstTeamScore = (TextView) view.findViewById(R.id.tv_first_team_score);
+        tvFirstTeamOvers = (TextView) view.findViewById(R.id.first_team_overs);
+        ivDwnSecond = (ImageView) view.findViewById(R.id.iv_down_second);
+        tvTeamSecondNameAndScore = (TextView) view.findViewById(R.id.tv_team_second_name);
+        tvSecondTeamOver = (TextView) view.findViewById(R.id.second_team_overs);
+        tvExtraRunTeamSecond = (TextView) view.findViewById(R.id.tv_extra_run_team_second);
+        tvTotalRunSecondTeam = (TextView) view.findViewById(R.id.tv_total_run_second_team);
+        tvRunRateSecondTeam = (TextView) view.findViewById(R.id.tv_run_rate_second_team);
+        tvSecondTeamScore = (TextView) view.findViewById(R.id.tv_second_team_score);
+        tvSecondTeamOvers = (TextView) view.findViewById(R.id.second_team_overs);
         initErrorLayout(view);
 
     }
@@ -105,5 +136,6 @@ public class LiveCricketMatchScoreCardFragment extends Fragment implements Lived
         }
 
     }
+
 
 }
