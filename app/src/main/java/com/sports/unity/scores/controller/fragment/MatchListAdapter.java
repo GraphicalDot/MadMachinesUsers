e1cf7c7965605d8a20244dc189603c27d27d5ad5
Ashish Katiyar <ashish@cfeindia.com>
2016-03-31 13:42:58 +0530

    bug fixes

--- app/src/main/java/com/sports/unity/scores/controller/fragment/MatchListAdapter.java
+++ app/src/main/java/com/sports/unity/scores/controller/fragment/MatchListAdapter.java
@@ -411,19 +411,19 @@ public class MatchListAdapter extends RecyclerView.Adapter<MatchListAdapter.View
         holder.liveText.setVisibility(View.VISIBLE);
         holder.matchDay.setText(cricketMatchJsonCaller.getMatchNumber());
         StringBuilder stringBuilder = new StringBuilder("");
-        stringBuilder.append(cricketMatchJsonCaller.getTeam1Score());
+        stringBuilder.append(cricketMatchJsonCaller.getTeam1Score()!=null?cricketMatchJsonCaller.getTeam1Score():"0");
         stringBuilder.append("/");
-        stringBuilder.append(cricketMatchJsonCaller.getWicketsTeam1());
+        stringBuilder.append(cricketMatchJsonCaller.getWicketsTeam1() != null ? cricketMatchJsonCaller.getWicketsTeam1():"0");
         holder.t1score.setText(stringBuilder.toString());
-        holder.team1Overs.setText(cricketMatchJsonCaller.getOversTeam1()+" ovs");
+        holder.team1Overs.setText(cricketMatchJsonCaller.getOversTeam1()!=null?cricketMatchJsonCaller.getOversTeam1():"0"+"ovs");
 
 
         stringBuilder = new StringBuilder("");
-        stringBuilder.append(cricketMatchJsonCaller.getTeam2Score());
+        stringBuilder.append(cricketMatchJsonCaller.getTeam2Score()!=null?cricketMatchJsonCaller.getTeam2Score():"0");
         stringBuilder.append("/");
-        stringBuilder.append(cricketMatchJsonCaller.getWicketsTeam2 ());
+        stringBuilder.append(cricketMatchJsonCaller.getWicketsTeam2()!=null?cricketMatchJsonCaller.getWicketsTeam2():"0");
         holder.t2score.setText(stringBuilder.toString());
-        holder.team2Overs.setText(cricketMatchJsonCaller.getOversTeam2()+" ovs");
+        holder.team2Overs.setText(cricketMatchJsonCaller.getOversTeam2()!=null?cricketMatchJsonCaller.getOversTeam2():"0"+"ovs");
     }
 
     private void setUpcommingMatchScoreCard(ViewHolder holder) throws JSONException {
