3f9e89b7db084f28dce62135a3573f9b1fab7df7
Ashish Katiyar <ashish@cfeindia.com>
2016-04-02 18:44:11 +0530

    remove mathes keys when unregister match

--- app/src/main/java/com/sports/unity/gcm/UnRegisterMatch.java
+++ app/src/main/java/com/sports/unity/gcm/UnRegisterMatch.java
@@ -2,9 +2,14 @@ package com.sports.unity.gcm;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.sports.unity.util.Constants;
+
+import org.json.JSONObject;
 
 
 /**
@@ -17,6 +22,7 @@ public class UnRegisterMatch extends IntentService implements TokenRegistrationH
 
 
     private TokenRegistrationHandler tokenRegistrationHandler;
+    private String matchId;
 
     public UnRegisterMatch() {
         super("UnRegisterMatch");
@@ -32,7 +38,7 @@ public class UnRegisterMatch extends IntentService implements TokenRegistrationH
 
     public void removeMatchUser(String matchId)
     {
-
+        this.matchId = matchId;
         tokenRegistrationHandler = TokenRegistrationHandler.getInstance(getApplicationContext());
         tokenRegistrationHandler.addListener(this);
         tokenRegistrationHandler.removeMatchUser(matchId);
@@ -40,6 +46,21 @@ public class UnRegisterMatch extends IntentService implements TokenRegistrationH
 
     @Override
     public void handleContent(String content) {
-        Log.i("Remove Match", "handleContent: "+content);
+        try{
+            Log.i("Remove Match", "handleContent: "+content);
+            JSONObject jsonObject = new JSONObject(content);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            if(jsonObject!=null && 200 == jsonObject.getInt("status") && jsonObject.getString("info").equalsIgnoreCase("success")){
+                SharedPreferences.Editor editor = preferences.edit();
+                if(TextUtils.isEmpty(preferences.getString(matchId, "")))
+                {
+                    editor.remove(matchId);
+
+                }
+                editor.apply();
+            }
+        }catch (Exception e){e.printStackTrace();}
+
+
     }
 }
