e38f30d770b6965907fbf3876b504a1519faeaed
Ashish Katiyar <ashish@cfeindia.com>
2016-04-14 14:16:32 +0530

    refresh time change

--- app/src/main/java/com/sports/unity/util/Constants.java
+++ app/src/main/java/com/sports/unity/util/Constants.java
@@ -113,7 +113,7 @@ public class Constants {
     public static final String INTENT_KEY_LEAGUE_ID = "league_id";
     public static final String INTENT_KEY_TEAM1_NAME = "team1";
     public static final String INTENT_KEY_TEAM2_NAME = "team2";
-    public static final long TIMEINMILISECOND = 30000;
+    public static final long TIMEINMILISECOND = 20000;
     public static final String ERRORRESPONSE = "{\"success\":false,\"error\":true}";
     public static final String TOKEN_PARAM = "token";
     public static final String INTENT_KEY_SERIES = "series_id";
