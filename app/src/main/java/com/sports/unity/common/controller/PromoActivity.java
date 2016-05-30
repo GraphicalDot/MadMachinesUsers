a8f15b917c0de61ce33c6cdbb1c40d86ae2b6ab4
agupta39 <anupam@madmachines.io>
2016-05-30 15:32:47 +0530

    Features added :
     - promo code screen working(on staging server).

--- app/src/main/java/com/sports/unity/common/controller/PromoActivity.java
+++ app/src/main/java/com/sports/unity/common/controller/PromoActivity.java
@@ -1,15 +1,31 @@
 package com.sports.unity.common.controller;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.sports.unity.BuildConfig;
 import com.sports.unity.R;
 import com.sports.unity.common.model.TinyDB;
 
@@ -32,7 +48,24 @@ import static com.sports.unity.util.CommonUtil.getDeviceId;
 
 public class PromoActivity extends CustomAppCompatActivity {
 
-    public static final String BASE_URL = "http://54.169.217.88/get_referral_code";
+    public static final String BASE_URL_GET_PROMO_CODE = "http://" + BuildConfig.XMPP_SERVER_BASE_URL + "/get_referral_code";
+    public static final String BASE_URL_AVAIL_PROMO_CODE = "http://" + BuildConfig.XMPP_SERVER_BASE_URL + "/redeem_code";
+    public static final String BASE_INVITE_USERS_URL = "https://play.google.com/store/apps/details?id=co.sports.unity&referrer=utm_source%3D";
+    public static final String END_INVITE_USERS_URL = "%26utm_medium%3Dsp-andr";
+
+
+    public static final String REFERRAL_CODE = "referral_code";
+
+    private ImageView promotionResultImage;
+    private TextView promotionResultText;
+    private LinearLayout promotionResultLayout;
+    private TextView addPromo;
+    private EditText promoCodeText;
+    private TextView ownPromoCode;
+    private TextView noPromoCodeText;
+    private LinearLayout sharePromoCodeLayout;
+    private CoordinatorLayout coordinatorLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +75,65 @@ public class PromoActivity extends CustomAppCompatActivity {
         initToolbar();
         initView();
         setPromoCode();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        ImageView backNavigation = (ImageView) toolbar.findViewById(R.id.backarrow);
+
+        toolbarTitle.setText("Promotions");
+        backNavigation.setOnClickListener(onClickListener);
+
+    }
+
+    private void initView() {
+        TextView detail = (TextView) findViewById(R.id.detail);
+        Button inviteFriends = (Button) findViewById(R.id.invite_frnds);
+
+        promoCodeText = (EditText) findViewById(R.id.promo_text);
+        addPromo = (TextView) findViewById(R.id.promo_code);
+        promotionResultImage = (ImageView) findViewById(R.id.promotion_result);
+        promotionResultText = (TextView) findViewById(R.id.promotion_text);
+        promotionResultLayout = (LinearLayout) findViewById(R.id.promotion_result_layout);
+        noPromoCodeText = (TextView) findViewById(R.id.no_promo_code_text);
+        sharePromoCodeLayout = (LinearLayout) findViewById(R.id.share_your_promo_code);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.root_coordinator_layout);
+
+        detail.setOnClickListener(onClickListener);
+        addPromo.setOnClickListener(onClickListener);
+        inviteFriends.setOnClickListener(onClickListener);
+
+        promoCodeText.clearFocus();
+        promoCodeText.setCursorVisible(false);
+
+        promoCodeText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                addPromo.setVisibility(View.VISIBLE);
+                promotionResultLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setPromoCode() {
+        ownPromoCode = (TextView) findViewById(R.id.own_promocode);
+        ownPromoCode.setVisibility(View.VISIBLE);
+        String promoCode = TinyDB.getInstance(getApplicationContext()).getString(TinyDB.PROMO_CODE);
+        if (promoCode.equals("") || promoCode == null) {
+            getOwnPromoCode();
+        } else {
+            ownPromoCode.setText(promoCode);
+        }
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -58,18 +150,16 @@ public class PromoActivity extends CustomAppCompatActivity {
                 case R.id.invite_frnds:
                     inviteFriends();
                     break;
+                case R.id.backarrow:
+                    PromoActivity.this.finish();
             }
         }
     };
 
-    private void availPromoCode() {
-        //TODO
-    }
-
     private void inviteFriends() {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, BASE_INVITE_USERS_URL + ownPromoCode.getText().toString() + END_INVITE_USERS_URL);
         sendIntent.setType("text/plain");
         startActivity(sendIntent);
     }
@@ -85,26 +175,6 @@ public class PromoActivity extends CustomAppCompatActivity {
         });
         AlertDialog dialog = builder.create();
         dialog.show();
-    }
-
-    private void initView() {
-        TextView detail = (TextView) findViewById(R.id.detail);
-        TextView addPromo = (TextView) findViewById(R.id.promo_code);
-        Button inviteFriends = (Button) findViewById(R.id.invite_frnds);
-
-        detail.setOnClickListener(onClickListener);
-        addPromo.setOnClickListener(onClickListener);
-        inviteFriends.setOnClickListener(onClickListener);
-    }
-
-    private void setPromoCode() {
-        TextView ownPromoCode = (TextView) findViewById(R.id.own_promocode);
-        String promoCode = TinyDB.getInstance(getApplicationContext()).getString(TinyDB.PROMO_CODE);
-        if (promoCode.equals("") || promoCode == null) {
-            getOwnPromoCode();
-        } else {
-            ownPromoCode.setText(promoCode);
-        }
     }
 
     private void getOwnPromoCode() {
@@ -126,16 +196,156 @@ public class PromoActivity extends CustomAppCompatActivity {
         return data.toString();
     }
 
-    private void initToolbar() {
+    private void availPromoCode() {
+        if (TextUtils.isEmpty(promoCodeText.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "please enter a refferal code", Toast.LENGTH_SHORT).show();
+        } else {
+            promoCodeText.clearFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
 
+
+            String promoDataAsJSON = getPromoDataAsJSON();
+            new AvailPromoCode().execute(promoDataAsJSON);
+        }
+    }
+
+    private String getPromoDataAsJSON() {
+        JSONObject data = new JSONObject();
+        try {
+            data.put(SettingsActivity.USERNAME_KEY, TinyDB.getInstance(getApplicationContext()).getString(KEY_USER_JID));
+            data.put(SettingsActivity.PASSWORD_KEY, TinyDB.getInstance(getApplicationContext()).getString(KEY_PASSWORD));
+            data.put(SettingsActivity.APK_VERSION, "1.0");
+            data.put(SettingsActivity.UDID, getDeviceId(getApplicationContext()));
+            data.put(REFERRAL_CODE, promoCodeText.getText().toString());
+            Log.i("data", data.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return data.toString();
+    }
+
+    class AvailPromoCode extends AsyncTask<String, Void, Void> {
+
+        int success = -1;
+        ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ProgressBar progressBar = new ProgressBar(PromoActivity.this);
+            progressBar.getIndeterminateDrawable().setColorFilter(PromoActivity.this.getResources().getColor(R.color.app_theme_blue), android.graphics.PorterDuff.Mode.MULTIPLY);
+
+            pDialog = new ProgressDialog(PromoActivity.this);
+            pDialog.setMessage("Availing...");
+            pDialog.show();
+            pDialog.setIndeterminateDrawable(progressBar.getIndeterminateDrawable());
+        }
+
+        @Override
+        protected Void doInBackground(String... data) {
+            HttpURLConnection httpURLConnection;
+            ByteArrayInputStream byteArrayInputStream;
+            ByteArrayOutputStream byteArrayOutputStream;
+            URL postPromoCode;
+            try {
+                postPromoCode = new URL(BASE_URL_AVAIL_PROMO_CODE);
+                Log.i("url", BASE_URL_AVAIL_PROMO_CODE);
+                httpURLConnection = (HttpURLConnection) postPromoCode.openConnection();
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setConnectTimeout(15000);
+                httpURLConnection.setRequestMethod("POST");
+
+                byteArrayInputStream = new ByteArrayInputStream(data[0].getBytes());
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                byte chunk[] = new byte[4096];
+                int read = 0;
+                while ((read = byteArrayInputStream.read(chunk)) != -1) {
+                    outputStream.write(chunk, 0, read);
+                }
+
+                if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                    byteArrayOutputStream = new ByteArrayOutputStream();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+
+                    byte chunk2[] = new byte[4096];
+                    int read2 = 0;
+                    while ((read2 = inputStream.read(chunk2)) != -1) {
+                        byteArrayOutputStream.write(chunk2, 0, read2);
+                    }
+
+                    String content = String.valueOf(byteArrayOutputStream.toString());
+                    JSONObject responseJsonContent = new JSONObject(content);
+                    if (responseJsonContent.getInt("status") == 200) {
+                        success = 1;
+                    } else {
+                        success = 0;
+                    }
+                } else {
+                    success = 0;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            pDialog.dismiss();
+            if (success == 1) {
+                onSuccesfulAvail();
+            } else if (success == -1) {
+                Toast.makeText(getApplicationContext(), "Check your internet connection and try again", Toast.LENGTH_SHORT).show();
+            } else {
+                onUnSuccesfulAvail();
+            }
+        }
+    }
+
+    private void onSuccesfulAvail() {
+        promotionResultLayout.setVisibility(View.VISIBLE);
+        addPromo.setVisibility(View.GONE);
+        promotionResultImage.setImageResource(R.drawable.ic_valid);
+        promotionResultText.setTextColor(getResources().getColor(R.color.green));
+        promotionResultText.setText("Congratulations");
+
+
+    }
+
+    private void onUnSuccesfulAvail() {
+        promotionResultLayout.setVisibility(View.VISIBLE);
+        addPromo.setVisibility(View.GONE);
+        promotionResultImage.setImageResource(R.drawable.ic_invalid);
+        promotionResultText.setTextColor(getResources().getColor(R.color.brick_red));
+        promotionResultText.setText("Invalid or Expired Code");
     }
 
 
     class GetPromoCode extends AsyncTask<String, Void, String> {
 
+        private boolean success = false;
+        private ProgressBar progressBar;
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            progressBar = (ProgressBar) findViewById(R.id.ownpromo_progress);
+            progressBar.setVisibility(View.VISIBLE);
+            ownPromoCode.setVisibility(View.GONE);
         }
 
         @Override
@@ -146,8 +356,8 @@ public class PromoActivity extends CustomAppCompatActivity {
             String ownPromoCode = null;
             URL postPrivacyData;
             try {
-                postPrivacyData = new URL(BASE_URL);
-                Log.i("url", BASE_URL);
+                postPrivacyData = new URL(BASE_URL_GET_PROMO_CODE);
+                Log.i("url", BASE_URL_GET_PROMO_CODE);
                 httpURLConnection = (HttpURLConnection) postPrivacyData.openConnection();
                 httpURLConnection.setDoInput(true);
                 httpURLConnection.setDoOutput(true);
@@ -177,6 +387,7 @@ public class PromoActivity extends CustomAppCompatActivity {
                     String content = String.valueOf(byteArrayOutputStream.toString());
                     JSONObject responseJsonContent = new JSONObject(content);
                     if (responseJsonContent.getInt("status") == 200) {
+                        success = true;
                         if (responseJsonContent.has("referral_code")) {
                             ownPromoCode = responseJsonContent.getString("referral_code");
                         } else {
@@ -203,9 +414,39 @@ public class PromoActivity extends CustomAppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            TinyDB.getInstance(getApplicationContext()).putString(TinyDB.PROMO_CODE, s);
-            setPromoCode();
+            progressBar.setVisibility(View.GONE);
+            if (success) {
+                onSuccesfulFetchPromoCode(s);
+            } else {
+                onUnSuccesfulFetchPromoCode();
+            }
         }
+    }
+
+    private void onSuccesfulFetchPromoCode(String s) {
+        noPromoCodeText.setVisibility(View.GONE);
+        sharePromoCodeLayout.setVisibility(View.VISIBLE);
+        TinyDB.getInstance(getApplicationContext()).putString(TinyDB.PROMO_CODE, s);
+        setPromoCode();
+    }
+
+    private void onUnSuccesfulFetchPromoCode() {
+        noPromoCodeText.setVisibility(View.VISIBLE);
+        sharePromoCodeLayout.setVisibility(View.GONE);
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, "No Internet connection", Snackbar.LENGTH_INDEFINITE)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getOwnPromoCode();
+                    }
+                });
+
+        snackbar.setActionTextColor(getResources().getColor(R.color.app_theme_blue));
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.WHITE);
+        snackbar.show();
     }
 
 }
